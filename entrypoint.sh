1c4dfad5e014d2942679ee17f3fb140274687eb7
Dabija Fanel <afanasie.dabija@titanium-soft.com>
2018-09-10 16:47:42 +0300

    Fixed build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-pm2 start npm -- start
+# npm run start -- > stdout.txt 2> stderr.txt &
+# sleep 40
 # shellcheck disable=SC2086
 exec /sbin/tini -g -- \
   wait-for --timeout "${WAIT_FOR_TIMEOUT:-10}" $WAIT_FOR_HOSTS -- \
-  ./node_modules/.bin/nightwatch "$@"
+  nightwatch "$@"
